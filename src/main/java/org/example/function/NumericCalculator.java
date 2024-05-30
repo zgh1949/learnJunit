730cd0015c5c0aa0e9098b76bda1c54d75c40a03
zgh1949 <zgh1949@outlook.com>
2024-05-30 09:01:48 +0800

    TDD2 红-绿-重构：绿阶段,测试方法基本通过，但是代码非常粗糙

--- src/main/java/org/example/function/NumericCalculator.java
+++ src/main/java/org/example/function/NumericCalculator.java
@@ -2,6 +2,21 @@ package org.example.function;
 
 public class NumericCalculator {
     public double eval(String expression) {
-        return 0.0;
+        final String[] data;
+        if (expression.contains("+")) {
+            data = expression.split("\\+");
+            return Double.parseDouble(data[0]) + Double.parseDouble(data[1]);
+        } else if (expression.contains("-")) {
+            data = expression.split("-");
+            return Double.parseDouble(data[0]) - Double.parseDouble(data[1]);
+        } else if (expression.contains("*")) {
+            data = expression.split("\\*");
+            return Double.parseDouble(data[0]) * Double.parseDouble(data[1]);
+        } else if (expression.contains("/")) {
+            data = expression.split("/");
+            return Double.parseDouble(data[0]) / Double.parseDouble(data[1]);
+        }
+
+        return 0.0D;
     }
 }
